8bd6f661a8561d913094dbc057e48c399b3205cd
Jason Wu <jason_wu@aai.com.tw>
2023-07-07 14:41:33 +0800

    Add initial user and password api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PasswordModule } from './password/password.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, PasswordModule],
   controllers: [AppController],
   providers: [AppService],
 })
