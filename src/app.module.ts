16904d8cf04ba2d0bf64298c30890cab3b610250
Jason Wu <jason_wu@aai.com.tw>
2023-07-07 15:08:51 +0800

    Add postgres database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PasswordModule } from './password/password.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, PasswordModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: process.env.PG_DATABASE_USER,
+      password: process.env.PG_DATABASE_PASSWORD,
+      database: 'password_manager_api',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+    PasswordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
