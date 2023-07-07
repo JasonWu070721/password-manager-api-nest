46533539daf9149d31c4e03372fe5a6a45646eba
Jason Wu <jason_wu@aai.com.tw>
2023-07-07 16:22:18 +0800

    Add Redis config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: '127.0.0.1:6379',
+      retryAttempts: 10,
+      retryDelay: 3000,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
